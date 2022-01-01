2ee4af439e454b464818a5e122cdd06133d0126b
Osman Abdiresaq <54283482+hamashoool@users.noreply.github.com>
2022-01-01 10:28:08 -0800

    seperated

--- mobile_o/app/context/context.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const AuthContext = React.createContext(undefined);
-export const TokenContext = React.createContext(undefined);
-export const DataContext = React.createContext(undefined);
