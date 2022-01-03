baa81a1b985ce78056d39dc3b67e1e530250b639
Osman Abdiresaq <54283482+hamashoool@users.noreply.github.com>
2022-01-03 11:43:28 +0600

    update 5

--- rescue_girls_api/rescue_girls_api/settings.py
+++ rescue_girls_api/rescue_girls_api/settings.py
@@ -22,7 +22,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECURITY_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
